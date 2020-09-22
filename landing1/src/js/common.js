ffd8310b3034e748ba0773c3a69b8f18cf31102b
Ivan Gorbynko <ivan.gorbynko@gmail.com>
2020-09-22 12:36:34 +0300

    added fonts

--- landing1/src/js/common.js
+++ landing1/src/js/common.js
@@ -5,7 +5,7 @@ import './../scss/common.scss';
 import $ from 'jquery';
 
 window.$ = window.jQuery = $;
-import SVGInjector from 'svg-injector';
+//import SVGInjector from 'svg-injector';
 
 import {photoGallery} from '../js/partials/photogallery';
 import {showcaseSlider} from '../js/partials/showcase_slider';
@@ -16,17 +16,17 @@ import {headerCollapse, headerScroll} from '../js/partials/header';
 import {videoPopup} from '../js/partials/video_popup';
 
 window.addEventListener('DOMContentLoaded', () => {
-    let mySVGsToInject = $('img.inject-me');
-    SVGInjector(mySVGsToInject);
+    //let mySVGsToInject = $('img.inject-me');
+    //SVGInjector(mySVGsToInject);
 
-    headerCollapse();
-    photoGallery();
-    locationMap();
-    contactsMap();
-    showcaseSlider();
-    aboutSlider();
-    planSlider();
-    videoPopup();
+    // headerCollapse();
+    // photoGallery();
+    // locationMap();
+    // contactsMap();
+    // showcaseSlider();
+    // aboutSlider();
+    // planSlider();
+    // videoPopup();
 
     $(window).on('scroll', function (){
         headerScroll()
