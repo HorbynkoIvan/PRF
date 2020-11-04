d6e06d4d8cf836077bad9597af66de589ced8c62
HorbynkoIvan <horbynko.i@gmail.com>
2020-11-04 14:16:54 +0200

    adaptive slider tablet

--- landingCleanBag/src/js/common.js
+++ landingCleanBag/src/js/common.js
@@ -6,7 +6,9 @@ import $ from 'jquery';
 
 window.$ = window.jQuery = $;
 import {estimateSlider} from './partials/estimate_slider.js'
+import {secretsSlider} from './partials/secret_slider'
 
 window.addEventListener('DOMContentLoaded', () => {
     estimateSlider()
+    secretsSlider()
 })
--- landingCleanBag/src/js/partials/secret_slider.js
+++ landingCleanBag/src/js/partials/secret_slider.js
@@ -1,17 +1,9 @@
-import slick from 'slick-carousel';
-
-export function estimateSlider() {
-    let $slider = $('.estimate .slider');
-
-    // $slider.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-    //     let i = (currentSlide ? currentSlide : 0) + 1;
-    //     $('.estimate .current').text(i);
-    //     $('.estimate .total').text(slick.slideCount);
-    // });
+export function secretsSlider() {
+    let $slider = $('.secrets .slider');
 
     $slider.slick({
         infinite: false,
-        slidesToShow: 3,
+        slidesToShow: 1,
         slidesToScroll: 1,
         prevArrow: '<svg class="arrow_left"><use xlink:href="/assets/img/arrow_sprite.svg#arrow_left"></use></svg>',
         nextArrow: '<svg class="arrow_right"><use xlink:href="/assets/img/arrow_sprite.svg#arrow_right"></use></svg>',
@@ -32,13 +24,4 @@ export function estimateSlider() {
             }
         ]
     });
-
-    // $('.estimate .prev').on('click', function (e) {
-    //     $slider.slick("slickPrev");
-    // })
-    // $('.estimate .next').on('click', function (e) {
-    //     $slider.slick("slickNext");
-    // })
-
-
 }
