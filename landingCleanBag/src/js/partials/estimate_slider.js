54d70f2a2633dccd968ffc5667ecf399aa1bc73b
Ivan Gorbynko <ivan.gorbynko@gmail.com>
2020-11-05 11:05:45 +0200

    added cdn

--- landingCleanBag/src/js/partials/estimate_slider.js
+++ landingCleanBag/src/js/partials/estimate_slider.js
@@ -13,8 +13,8 @@ export function estimateSlider() {
         infinite: false,
         slidesToShow: 3,
         slidesToScroll: 1,
-        prevArrow: '<svg class="arrow_left"><use xlink:href="/assets/img/arrow_sprite.svg#arrow_left"></use></svg>',
-        nextArrow: '<svg class="arrow_right"><use xlink:href="/assets/img/arrow_sprite.svg#arrow_right"></use></svg>',
+        prevArrow: '<svg class="arrow_left"><use xlink:href="https://cdn.parfums.ua/public/upload/newAdminUpload/RichContent/900542/arrow_sprite.svg#arrow_left"></use></svg>',
+        nextArrow: '<svg class="arrow_right"><use xlink:href="https://cdn.parfums.ua/public/upload/newAdminUpload/RichContent/900542/arrow_sprite.svg#arrow_right"></use></svg>',
         responsive: [
             {
                 breakpoint: 1024,
